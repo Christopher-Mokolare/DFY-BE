50d9c28fcb0e6ce61cd99f6d96a46fbb57d3f640
OBAKENG Christopher-Mokolare <2co.mokolare@gmail.com>
2026-03-31 13:38:54 +0200

    fix: add SyncSchemaWithModel migration, CommissionPercentage column in DB

---
+++ Migrations/20260331113745_SyncSchemaWithModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoForYou.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncSchemaWithModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Tasks",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Tasks",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TaskName",
+                table: "Tasks",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "TaskName",
+                table: "Tasks");
+        }
+    }
+}
